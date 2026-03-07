abae0cd1c8a218d3db9213f1b8e1724bcbf49c33
Justin <jpf99@yahoo.com>
2026-03-06 20:13:18 -0500

    testing a different curve above footer, seeing if i like it

--- components/Hosting.tsx
+++ components/Hosting.tsx
@@ -66,9 +66,9 @@ const Hosting: React.FC = () => {
         <svg
           viewBox="0 0 1200 120"
           preserveAspectRatio="none"
-          className="relative block w-[calc(100%+4px)] -ml-[2px] h-[80px] fill-primary transition-transform duration-300 rotate-180"
+          className="relative block w-[calc(100%+4px)] -ml-[2px] h-[120px] md:h-[150px] fill-primary transition-transform duration-300 rotate-180"
         >
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"></path>
+          <path d="M0,0 V40 Q600,200 1200,40 V0 Z"></path>
         </svg>
       </div>
     </section>
