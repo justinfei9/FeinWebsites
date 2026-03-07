47a4405dbec7764f7e19bb6f186a66a171aa0f95
Justin <jpf99@yahoo.com>
2026-03-06 19:56:40 -0500

    mobile fix to footer transition to other pages

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -277,7 +277,7 @@ const Contact: React.FC = () => {
             </motion.div>
 
             {/* ── MOUNTAIN RANGE + FOOTER CURVE ── */}
-            <div className="absolute bottom-0 left-0 w-full pointer-events-none z-10">
+            <div className="absolute -bottom-1 left-0 w-full pointer-events-none z-10">
                 <svg
                     viewBox="0 0 1440 260"
                     preserveAspectRatio="none"
--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -244,7 +244,7 @@ const Pricing: React.FC = () => {
             </motion.div>
 
             {/* ── MOUNTAIN RANGE + FOOTER CURVE ── */}
-            <div className="absolute bottom-0 left-0 w-full pointer-events-none z-10">
+            <div className="absolute -bottom-1 left-0 w-full pointer-events-none z-10">
                 <svg
                     viewBox="0 0 1440 260"
                     preserveAspectRatio="none"
